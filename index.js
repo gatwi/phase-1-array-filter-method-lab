c6ac2886aa8388bf00eab7eb4bc511a5f0bf75cf
gatwi <faith.murithi@student.moringaschool.com>
2022-06-10 23:15:53 +0300

    commit to master

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+// // Code your solution here
+// function findMatching (source, sought) {
+//     const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+//     return SourceBuffer.filter (possibleMatch); possibleMatch.toLowerCase () === sought.toLowerCase ()
+// }
+
+
+function findMatching(source, sought) {
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+}
+
+function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+}
